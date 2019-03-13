e4f4a5bd8943860632e44b5c811bf8e5e4af435e
chiic <chiic1101@163.com>
2019-03-13 20:33:10 +0800

    修改origin

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ config = {
     hostname: '0.0.0.0',
     port: 3001,
     log_dir: path.join(__dirname, 'log_dir'),
-    allowOrigin: 'http://chiicx.com:8080'
+    allowOrigin: 'http://book.chiicx.com'
 }
 
 module.exports = config;
